899ebcc02edbb8d108f61b2989a7b77c40d6d4e4
Pablo Olivera <p.oliverav@gmail.com>
2021-04-20 10:03:46 -0300

    Initial commit

---
+++ webpack.base.js
@@ -0,0 +1,84 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { ModuleFederationPlugin } = require('webpack').container;
+
+const processPath = process.cwd();
+
+const SRC_PATH = path.resolve(processPath, 'src');
+const DIST_PATH = path.resolve(processPath, 'dist');
+const PKG_JSON_FILE = path.resolve(SRC_PATH, 'package.json');
+
+const {
+  name: packageName,
+  main: entryFileName,
+  environment = 'development',
+  shared,
+  remotes,
+  exposes,
+// eslint-disable-next-line import/no-dynamic-require
+} = require(PKG_JSON_FILE);
+const ENTRY_FILE = path.resolve(SRC_PATH, entryFileName);
+
+/**
+ * Camelize given string
+ * @returns string
+ * @param str string
+ */
+const camelize = (str) => (
+  str.replace(/(?:^\w|[A-Z]|-|\b\w|\s+)/g, (match, index) => {
+    if (match === ' ' || match === '-') return '';
+    return (index === 0) ? match.toLowerCase() : match.toUpperCase();
+  })
+);
+const PACKAGE_NAME = camelize(packageName);
+
+module.exports = {
+  context: SRC_PATH,
+  entry: ENTRY_FILE,
+  mode: environment,
+  resolve: {
+    extensions: ['.jsx', '.js', '.json'],
+  },
+  output: {
+    path: DIST_PATH,
+    filename: ENTRY_FILE,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.jsx?$/,
+        include: path.resolve(__dirname, 'src'),
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env', '@babel/preset-react'],
+        },
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]',
+          outputPath: 'public/fonts/',
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin(
+      { template: path.resolve(SRC_PATH, 'index.html') },
+    ),
+    new ModuleFederationPlugin({
+      name: PACKAGE_NAME,
+      library: { type: 'var', name: PACKAGE_NAME },
+      filename: 'remoteEntry.js',
+      exposes,
+      shared,
+      remotes,
+    }),
+  ],
+};
