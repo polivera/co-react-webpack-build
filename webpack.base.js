cb9e6446be19fa9f9d62379c61a5fc7aa9ca61e7
Pablo Olivera <p.oliverav@outlook.com>
2022-06-04 16:28:55 -0300

    feat(fonts): Adding proper font configuration

--- webpack.base.js
+++ webpack.base.js
@@ -1,35 +1,35 @@
-const { resolve } = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const { ModuleFederationPlugin } = require("webpack").container;
+const { resolve } = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { ModuleFederationPlugin } = require('webpack').container;
 
 const processPath = process.cwd();
-const PKG_JSON_FILE = resolve(processPath, "package.json");
+const PKG_JSON_FILE = resolve(processPath, 'package.json');
 
 /**
  * Extract all the information from the package.json file
  */
 const {
+  allowedHosts = 'all',
   name: packageName,
-  main: entryFileName = "index.ts",
-  fonts: fontConfig,
-  sourceFolder = "./src",
-  host = "localhost",
+  main: entryFileName = 'index.ts',
+  sourceFolder = './src',
+  host = 'localhost',
   port = 9000,
   shared = [],
   remotes = [],
   exposes = [],
-  dependencies: deps
-// todo: is there a better way to do this?
-// eslint-disable-next-line import/no-dynamic-require
+  dependencies: deps,
+  // todo: is there a better way to do this?
+  // eslint-disable-next-line import/no-dynamic-require
 } = require(PKG_JSON_FILE);
+const path = require('path');
 
 /**
  * Resolve paths and stuff
  */
 const SRC_PATH = resolve(processPath, sourceFolder);
 const ENTRY_FILE_PATH = resolve(SRC_PATH, entryFileName);
-const PUBLIC_PATH = resolve(SRC_PATH, "public");
-const DIST_PATH = resolve(processPath, "./dist");
+const DIST_PATH = resolve(processPath, './dist');
 
 /**
  * Camelize given string
@@ -38,14 +38,17 @@ const DIST_PATH = resolve(processPath, "./dist");
  */
 const camelize = (str) =>
   str.replace(/^\w|[A-Z]|-|\b\w|\s+/g, (match, index) => {
-    if (match === " " || match === "-") return "";
+    if (match === ' ' || match === '-') return '';
     return index === 0 ? match.toLowerCase() : match.toUpperCase();
   });
 const PACKAGE_NAME = camelize(packageName);
-// eslint-disable-next-line no-console
-console.info(
-  `Remote URL: ${PACKAGE_NAME}@http://${host}:${port}/remoteEntry.js`
-);
+
+if (Object.keys(exposes).length > 0) {
+  // eslint-disable-next-line no-console
+  console.info(
+    `Remote URL: ${PACKAGE_NAME}@http://${host}:${port}/remoteEntry.js`
+  );
+}
 
 /**
  * Add version to the shared libraries based on your dependencies
@@ -60,34 +63,30 @@ const sharedWithVersions = Object.fromEntries(
 const rules = {
   css: {
     test: /\.css$/i,
-    use: ["style-loader", "css-loader"],
+    use: ['style-loader', 'css-loader'],
   },
   jsx: {
     test: /\.(ts|tsx|js|jsx)$/,
     include: [SRC_PATH],
     exclude: /node_modules/,
-    loader: "babel-loader",
+    loader: 'babel-loader',
     options: {
-      presets: ["@babel/preset-env", "@babel/preset-react", "@babel/preset-typescript"],
-      plugins: ["@babel/transform-runtime"]
+      presets: [
+        ['@babel/preset-env', { modules: 'auto' }],
+        '@babel/preset-react',
+        '@babel/preset-typescript',
+      ],
+      plugins: ['@babel/transform-runtime'],
     },
   },
   fonts: {
-    test: /\.(woff(2)?|ttf|eot)(\?v=\d+\.\d+\.\d+)?$/,
-    loader: "file-loader",
-    include: fontConfig?.include
-      ? resolve(processPath, fontConfig.include)
-      : resolve(PUBLIC_PATH, "fonts"),
-    options: {
-      name: "[name].[ext]",
-      outputPath: fontConfig?.outputPath
-        ? resolve(processPath, fontConfig.outputPath)
-        : "dist/public",
-    },
+    test: /\.(ttf|eot|woff|woff2)$/i,
+    type: 'asset',
   },
+  // todo: Check if this should be asset or asset/resource
   images: {
     test: /\.(png|jpg|jpeg|gif)$/i,
-    type: "asset/resource",
+    type: 'asset',
   },
   svg: {
     test: /\.svg$/,
@@ -99,37 +98,45 @@ const rules = {
         },
       },
     ],
-  }
+  },
 };
 
 const webpackConfig = {
-  mode: "none",
+  mode: 'none',
   context: SRC_PATH,
   entry: ENTRY_FILE_PATH,
+  devServer: {
+    static: {
+      directory: path.resolve(processPath, 'dist'),
+    },
+    host,
+    port,
+    open: true,
+    hot: true,
+    compress: true,
+    allowedHosts,
+  },
   resolve: {
-    extensions: [".jsx", ".js", ".json", ".tsx", ".ts"],
+    extensions: ['.jsx', '.js', '.json', '.tsx', '.ts'],
   },
   output: {
     path: DIST_PATH,
-    filename: "[name][contenthash].js",
+    filename: '[name][contenthash].js',
+    clean: true,
   },
   module: {
-    rules: [rules.css, rules.jsx, rules.images, rules.svg],
+    rules: [rules.css, rules.jsx, rules.images, rules.svg, rules.fonts],
   },
   plugins: [
-    new HtmlWebpackPlugin({ template: resolve(processPath, "index.html") }),
+    new HtmlWebpackPlugin({ template: resolve(processPath, 'index.html') }),
     new ModuleFederationPlugin({
       name: PACKAGE_NAME,
-      filename: "remoteEntry.js",
+      filename: 'remoteEntry.js',
       exposes,
       shared: sharedWithVersions,
       remotes,
     }),
   ],
 };
-
-if (fontConfig) {
-  webpackConfig.module.rules = [...webpackConfig.module.rules, rules.fonts];
-}
 
 module.exports = webpackConfig;
--- webpack.production.js
+++ webpack.production.js
@@ -1,8 +1,21 @@
 const { merge } = require('webpack-merge');
+// const { DefinePlugin } = require('webpack');
 const base = require('./webpack.base');
 
 const productionConfig = {
-  mode: "production",
+  mode: 'production',
+  devtool: 'source-map',
+  devServer: {
+    open: false,
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+    },
+  },
+  performance: {
+    hints: 'error',
+  },
 };
 
 module.exports = merge(base, productionConfig);
